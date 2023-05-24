aadc51364576aea64fc4655a6767400c52617ab5
Quan <quan.dn95@gmail.com>
2023-05-24 16:30:40 -0700

    added css for reviews

--- Reviews/src/App.jsx
+++ Reviews/src/App.jsx
@@ -55,7 +55,7 @@ const App = () => {
             <FaChevronLeft />
           </button>
           <button
-            className='prev-btn'
+            className='next-btn'
             onClick={() => nextPerson()}
           >
             <FaChevronRight />
