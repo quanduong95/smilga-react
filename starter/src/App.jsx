0f9ac4e5d08cf127e8aa58e5229a468cd1660c0e
Quan <quan.dn95@gmail.com>
2023-05-24 14:47:22 -0700

    added logic for prev and next

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -6,6 +6,31 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const nextPerson = () => {
+    setIndex((curr) => {
+      return curr === people.length - 1 ? 0 : curr + 1;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((curr) => {
+      return curr === 0 ? people.length - 1 : curr - 1;
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNum = Math.floor(Math.random() * people.length);
+    if (randomNum === index) {
+      randomNum++;
+    }
+    if (randomNum > people.length - 1) {
+      randomNum = 0;
+    } else if (randomNum === 0) {
+      randomNum = people.length - 1;
+    }
+    setIndex(randomNum);
+  };
+
   return (
     <main>
       <article className='review'>
@@ -22,6 +47,26 @@ const App = () => {
         <h4 className='author'>{name}</h4>
         <p className='job'>{job}</p>
         <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button
+            className='prev-btn'
+            onClick={() => prevPerson()}
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            className='prev-btn'
+            onClick={() => nextPerson()}
+          >
+            <FaChevronRight />
+          </button>
+        </div>
+        <button
+          className='btn btn-hipster'
+          onClick={() => randomPerson()}
+        >
+          surprise me
+        </button>
       </article>
     </main>
   );
