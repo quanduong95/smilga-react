478d7ae1e0645e385907d087d4e435b910bfe583
Quan <quan.dn95@gmail.com>
2023-05-24 19:29:49 -0700

    css for questions

--- Questions/src/components/QuestionList.jsx
+++ Questions/src/components/QuestionList.jsx
@@ -4,6 +4,7 @@ import Question from './Question';
 const QuestionList = ({ questions }) => {
   return (
     <section className='container'>
+      <h1>Questions</h1>
       {questions.map((question) => (
         <Question
           key={question.id}
