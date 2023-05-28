6b616f64f4b630a5d39dc3e8c7c140701e0dc9f5
Quan <quan.dn95@gmail.com>
2023-05-27 23:53:26 -0700

    done css for Tabs

--- Tabs/src/components/Duties.jsx
+++ Tabs/src/components/Duties.jsx
@@ -4,18 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Duties = ({ duties }) => {
   return (
-    <div>
+    <>
       {duties.map((duty) => {
         return (
           <div
             key={uuidv4()}
             className='job-desc'
           >
-            <FaAngleDoubleRight className='job-icon' /> <p>{duty}</p>
+            <FaAngleDoubleRight className='job-icon' />{' '}
+            <p className='job-duty'>{duty}</p>
           </div>
         );
       })}
-    </div>
+    </>
   );
 };
 
