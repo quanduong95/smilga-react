296c24f95f75208caad66e602bb443e180e35f4a
Quan <quan.dn95@gmail.com>
2023-06-10 20:23:58 -0700

    set up data and some styling

---
+++ NavBar/src/App.jsx
@@ -0,0 +1,9 @@
+import NavBar from './components/NavBar';
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+export default App;
---
+++ NavBar/src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import { FaBars } from 'react-icons/fa';
+import { links, social } from '../data';
+import logo from '../assets/logo.svg';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  return (
+    <section className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img
+            src={logo}
+            className='logo'
+            alt='logo'
+          />
+          <button
+            className=' nav-toggle'
+            onClick={toggleLinks}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className='links-container'>
+            <ul className='links'>
+              {links.map((link) => (
+                <li key={nanoid()}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default NavBar;
